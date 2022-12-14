e36e7cd85f127e666b102369a9bc1516a9948bf3
jameschapman19 <james.chapman.19@ucl.ac.uk>
2022-12-14 11:00:55 +0000

    Fixed Kernel method and Slightly Improved example with mse

--- ccammr/__init__.py
+++ ccammr/__init__.py
@@ -4,7 +4,7 @@ from typing import Iterable, Union
 import cvxpy as cp
 import numpy as np
 from cca_zoo.models import CCA
-from cca_zoo.utils import _check_views
+from cca_zoo.utils import _check_views, _process_parameter
 from sklearn.metrics import pairwise_kernels
 from sklearn.utils.validation import check_is_fitted
 
@@ -34,7 +34,26 @@ class CCAMMR(CCASample):
             latent_dims=1,
             C=0.0,
             norm='fro',
+            normalise_rows=True,
     ):
+        """
+        Parameters
+        ----------
+        scale : bool
+            Whether to scale the data to unit variance
+        centre : bool
+            Whether to centre the data
+        copy_data : bool
+            Whether to copy the data
+        random_state : int
+            Random state for initialisation
+        latent_dims : int
+            Number of latent dimensions to use
+        C : float
+            Regularisation parameter
+        norm : str
+            Norm to use for regularisation
+        """
         super().__init__(
             latent_dims=latent_dims,
             scale=scale,
@@ -44,15 +63,16 @@ class CCAMMR(CCASample):
         )
         self.C = C
         self.norm = norm
+        self.normalise_rows = normalise_rows
 
     def fit(self, views: Iterable[np.ndarray], y=None, **kwargs):
         views = self._validate_inputs(views)
+        if self.normalise_rows:
+            views = [view / np.linalg.norm(view, axis=1, keepdims=True) for view in views]
         self.weights, self.psi = self.primal_solve(*views, C=self.C)
         return self
 
     def primal_solve(self, X, Y, C=0.0):
-        X/= np.linalg.norm(X, axis=1, keepdims=True)
-        Y/= np.linalg.norm(Y, axis=1, keepdims=True)
         n = X.shape[0]
         p = X.shape[1]
         q = Y.shape[1]
@@ -73,7 +93,8 @@ class CCAMMR(CCASample):
 
     def transform(self, views: Iterable[np.ndarray], **kwargs):
         views = self._validate_inputs(views)
-        views=[view/np.linalg.norm(view, axis=1,keepdims=True) for view in views]
+        if self.normalise_rows:
+            views = [view / np.linalg.norm(view, axis=1, keepdims=True) for view in views]
         views[0] = views[0] @ self.weights
         return views
 
@@ -101,7 +122,34 @@ class CCAKMMR(CCAMMR):
             degree: Iterable[float] = None,
             coef0: Iterable[float] = None,
             kernel_params: Iterable[dict] = None,
+            normalise_rows=True,
     ):
+        """
+
+        Parameters
+        ----------
+        scale : bool
+            Whether to scale the data to unit variance
+        centre : bool
+            Whether to centre the data
+        copy_data : bool
+            Whether to copy the data
+        random_state : int
+            Random state for initialisation
+        latent_dims : int
+            Number of latent dimensions to use
+        C : float
+            Regularisation parameter
+        norm : str
+            Norm to use for regularisation
+        kernel : Union[float, callable]
+            Kernel to use
+        gamma : float
+            Kernel coefficient for rbf, poly and sigmoid kernels
+        degree : float
+            Degree for poly kernels
+        coef0 : float
+        """
         super().__init__(
             scale=scale,
             centre=centre,
@@ -109,6 +157,7 @@ class CCAKMMR(CCAMMR):
             random_state=random_state,
             C=C,
             norm=norm,
+            normalise_rows=normalise_rows,
         )
         self.kernel_params = kernel_params
         self.gamma = gamma
@@ -116,17 +165,28 @@ class CCAKMMR(CCAMMR):
         self.kernel = kernel
         self.degree = degree
 
+    def _check_params(self):
+        self.kernel = _process_parameter("kernel", self.kernel, "linear", 1)
+        self.gamma = _process_parameter("gamma", self.gamma, None, 1)
+        self.coef0 = _process_parameter("coef0", self.coef0, 1, 1)
+        self.degree = _process_parameter("degree", self.degree, 1, 1)
+        self.c = _process_parameter("c", self.c, 0, 1)
+
     def fit(self, views: Iterable[np.ndarray], y=None, **kwargs):
         views = self._validate_inputs(views)
+        if self.normalise_rows:
+            views = [view / np.linalg.norm(view, axis=1, keepdims=True) for view in views]
+        self._check_params()
         self.train_views = views
-        self.alpha = self.dual_solve(*views, C=self.C)
+        Kx = self._get_kernel(0, self.train_views[0])
+        self.weights = self.dual_solve(Kx,self.train_views[1], C=self.C)
         return self
 
     @staticmethod
-    def dual_solve(Kx, Ky, C=0.0):
+    def dual_solve(Kx, y, C=0.0):
         n = Kx.shape[0]
         alpha = cp.Variable(n)
-        Q = Kx * Ky
+        Q = Kx * (y@y.T)
         constraints = [alpha <= C, alpha >= 0]
         prob = cp.Problem(
             cp.Minimize(0.5 * cp.quad_form(alpha, Q) - cp.sum(alpha)), constraints
@@ -134,29 +194,27 @@ class CCAKMMR(CCAMMR):
         prob.solve()
         return alpha.value
 
-    def _get_kernel(self, X, Y=None):
-        if callable(self.kernel):
-            params = self.kernel_params or {}
+    def _get_kernel(self, view, X, Y=None):
+        if callable(self.kernel[view]):
+            params = self.kernel_params[view] or {}
         else:
-            params = {"gamma": self.gamma, "degree": self.degree, "coef0": self.coef0}
+            params = {
+                "gamma": self.gamma[view],
+                "degree": self.degree[view],
+                "coef0": self.coef0[view],
+            }
         return pairwise_kernels(
-            X, Y, metric=self.kernel, filter_params=True, **params
+            X, Y, metric=self.kernel[view], filter_params=True, **params
         )
 
     def transform(self, views: np.ndarray, **kwargs):
         check_is_fitted(self, attributes=["weights"])
-        views = _check_views(
-            *views, copy=self.copy_data, accept_sparse=self.accept_sparse
-        )
-        views = self._centre_scale_transform(views)
-        Ktest = [
-            self._get_kernel(i, self.train_views[i], Y=view)
-            for i, view in enumerate(views)
-        ]
-        transformed_views = [
-            kernel.T @ self.weights[i] for i, kernel in enumerate(Ktest)
-        ]
-        return transformed_views
+        views = self._validate_inputs(views)
+        if self.normalise_rows:
+            views = [view / np.linalg.norm(view, axis=1, keepdims=True) for view in views]
+        Kx = self._get_kernel(0, self.train_views[0], Y=views[0])
+        y_pred = Kx.T @ (self.train_views[1].T*self.weights).T
+        return y_pred, views[1]
 
 
 class LCA(CCASample):
--- scripts/example.py
+++ scripts/example.py
@@ -1,45 +1,57 @@
-from ccammr import CCAMMR, CCASample
+from ccammr import CCAMMR, CCASample, CCAKMMR
 import numpy as np
+from sklearn.metrics import mean_squared_error
 
-#example comparing CCAMMR and CCASample for finding effects in
-#subsets of data with different distributions based on their learnt
-#sample weights
+# example comparing CCAMMR and CCASample for finding effects in
+# subsets of data with different distributions based on their learnt
+# sample weights
 def main():
-    #generate data
+    # generate data
     n = 200
-    p = 5
-    q = 5
-    noise=1e-10
-    z= np.ones((n//2, 1))
-    wx= np.random.normal(size=(p, 1))
-    wy= np.random.normal(size=(q, 1))
-    X=z@wx.T + np.random.normal(0,noise,size=(n//2, p))
-    Y=z@wy.T + np.random.normal(0,noise,size=(n//2, q))
+    p = 10
+    q = 10
+    noise = 1e-10
+    z = np.ones((n // 2, 1))
+    wx = np.random.normal(size=(p, 1))
+    wy = np.random.normal(size=(q, 1))
+    X = z @ wx.T + np.random.normal(0, noise, size=(n // 2, p))
+    Y = z @ wy.T + np.random.normal(0, noise, size=(n // 2, q))
 
-    #standardise
-    X=(X-X.mean(axis=0))/X.std(axis=0)
-    Y=(Y-Y.mean(axis=0))/Y.std(axis=0)
-    X_noise=np.random.normal(size=(n//2, p))
-    Y_noise=np.random.normal(size=(n//2, q))
-    #standardise
-    X_noise=(X_noise-X_noise.mean(axis=0))/X_noise.std(axis=0)
-    Y_noise=(Y_noise-Y_noise.mean(axis=0))/Y_noise.std(axis=0)
-    #combine X and Y with noise
-    X=np.concatenate((X, X_noise))
-    Y=np.concatenate((Y, Y_noise))
+    # standardise
+    X = (X - X.mean(axis=0)) / X.std(axis=0)
+    Y = (Y - Y.mean(axis=0)) / Y.std(axis=0)
+    X_noise = np.random.normal(size=(n // 2, p))
+    Y_noise = np.random.normal(size=(n // 2, q))
+    # standardise
+    X_noise = (X_noise - X_noise.mean(axis=0)) / X_noise.std(axis=0)
+    Y_noise = (Y_noise - Y_noise.mean(axis=0)) / Y_noise.std(axis=0)
+    # combine X and Y with noise
+    X = np.concatenate((X, X_noise))
+    Y = np.concatenate((Y, Y_noise))
 
-    #fit models
-    #This is the Maximum Margin CCA Model where the sample weights are whether the dot product of the transformed X and the original Y
-    #is greater than 1
-    mmr = CCAMMR(C=1000000)
+    # fit models
+    # This is the Maximum Margin CCA Model where the sample weights are whether the dot product of the transformed X and the original Y
+    # is greater than 1
+    mmr = CCAMMR(C=10)
     mmr.fit([X, Y])
-    #This is a normal CCA model where the sample weights are whether the product of the latent variables is greater than 0
+    # Fit a Kernel Maximum Margin CCA Model with linear Kernel
+    kmmr = CCAKMMR(C=10, kernel='linear')
+    kmmr.fit([X, Y])
+    # This is a normal CCA model where the sample weights are whether the product of the latent variables is greater than 0
     cca = CCASample()
     cca.fit([X, Y])
 
-    #print sample weights. The first half of the sample weights (those with the signal) should be true and the second half false (those with the noise)
-    print(mmr.sample_weights((X,Y)))
-    print(cca.sample_weights((X,Y)))
+    #Print MSE for mmr and kmmr between transformed X and original Y for the first 100 samples and the last 100 samples
+    print('MMR MSE Signal: ', mean_squared_error(*mmr.transform([X[:100], Y[:100]])))
+    print('KMMR MSE Signal: ', mean_squared_error(*kmmr.transform([X[:100], Y[:100]])))
+    print('MMR MSE Signal: ', mean_squared_error(*mmr.transform([X[100:], Y[100:]])))
+    print('KMMR MSE Signal: ', mean_squared_error(*kmmr.transform([X[100:], Y[100:]])))
+
+    # print sample weights. The first half of the sample weights (those with the signal) should be true and the second half false (those with the noise)
+    print(mmr.sample_weights((X, Y)))
+    print(kmmr.sample_weights((X, Y)))
+    print(cca.sample_weights((X, Y)))
+
 
 if __name__ == '__main__':
     main()
